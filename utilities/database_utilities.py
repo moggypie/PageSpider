b6cb141f8ebeae05842245a99d4eb2d6d3b9d17e
Fi <44867393+moggypie@users.noreply.github.com>
2022-01-03 11:49:12 +0000

    fixed frmatting errors

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,7 +1,8 @@
 def create_database(database_path: str):
-    #TODO: generate database
+    # TODO: generate database
     pass
 
-def save_words_to_database(database_path:str, words_list: list):
-    #TODO save words to database
+
+def save_words_to_database(database_path: str, words_list: list):
+    # TODO save words to database
     pass
--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,11 +1,13 @@
-def load_urls_from_file(filepath: str):
-    #TODO: add the code needed to read the text file with the urls in it
+def load_urls_from_file(file_path: str):
+    # TODO: add the code needed to read the text file with the urls in it
     pass
+
 
 def load_page(url: str):
-    #TODO: add code that reads the url
+    # TODO: add code that reads the url
     pass
 
+
 def scrape_page(page_contents: str):
-    #TODO: analyse text
+    # TODO: analyse text
     pass
