9664dc85f8285a7371962d0d85feeeffd0f23237
Fi <44867393+moggypie@users.noreply.github.com>
2022-01-03 13:19:44 +0000

    update

--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,11 +1,20 @@
+from urllib.request import urlopen
+
+
 def load_urls_from_file(file_path: str):
-    # TODO: add the code needed to read the text file with the urls in it
-    pass
+    try:
+        with open(file_path) as f:
+            content = f.readline()
+            return content
+    except FileNotFoundError:
+        print("the file " + file_path + " could not be found")
+        exit(2)
 
 
 def load_page(url: str):
-    # TODO: add code that reads the url
-    pass
+    response = urlopen(url)
+    html = response.read().decode('utf-8')
+    return
 
 
 def scrape_page(page_contents: str):
